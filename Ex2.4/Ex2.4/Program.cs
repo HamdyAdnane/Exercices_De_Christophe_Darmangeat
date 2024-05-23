3e891cae0c083e9b8d71bf369fb85f95e49438da
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 16:56:36 +0100

    Add files via upload

---
+++ Ex2.4/Ex2.4/Program.cs
@@ -0,0 +1,15 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int Prix_Hors_Taxe, Nombre_Articles, Taux_TVA, Prix_Total_TTC;
+        Console.WriteLine("Entrez le prix hors taxes :");
+        Prix_Hors_Taxe= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Entrez le nombre d’articles :");
+        Nombre_Articles= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Entrez le taux de TVA :");
+        Taux_TVA= Convert.ToInt32(Console.ReadLine());
+        Prix_Total_TTC = Nombre_Articles * Prix_Hors_Taxe * (1+ Taux_TVA);
+        Console.WriteLine("Le prix toutes taxes est : "+ Prix_Total_TTC);
+    }
+}

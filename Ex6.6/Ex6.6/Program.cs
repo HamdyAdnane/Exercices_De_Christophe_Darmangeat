984118725ad56d32c4a998b8b2d43234f0201815
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:32:50 +0100

    Add files via upload

---
+++ Ex6.6/Ex6.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Suite = new int[8];
+        Suite[0] = 1;
+        Suite[1] = 1;
+        for (int i = 2; i <= 7; i++)
+        {
+            Suite[i] = Suite[i - 1] + Suite[i - 2];
+        }
+        for (int i = 0; i <= 7; i++)
+        {
+            Console.WriteLine(Suite[i]);
+        }
+    }
+}

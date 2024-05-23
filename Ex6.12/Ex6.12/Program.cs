b18c29ff548527186e842cd63e4760ae0e76c838
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:39:55 +0100

    Add files via upload

---
+++ Ex6.12/Ex6.12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Nb;
+        int[] T;
+        Console.WriteLine("Entrez le nombre de valeurs : ");
+        Nb = Convert.ToInt16(Console.ReadLine());
+        T = new int[Nb];
+        
+        for (int i = 0; i <= Nb - 1; i++)
+        {
+            Console.WriteLine("Entrez le nombre n° "+ (i + 1));
+            T[i] = Convert.ToInt16(Console.ReadLine());
+        }
+        Console.WriteLine("Nouveau tableau : ");
+        for (int i = 0; i <= Nb - 1; i++)
+        {
+            T[i] = T[i] + 1;
+            Console.WriteLine(T[i]);
+        }
+    }
+}

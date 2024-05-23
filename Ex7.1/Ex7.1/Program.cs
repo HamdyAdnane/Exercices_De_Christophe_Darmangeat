e5601aeef7ad3a78704e7e749956342564e4585a
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:47:45 +0100

    Add files via upload

---
+++ Ex7.1/Ex7.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Nb, i;
+        bool Flag;
+        int[] T;
+        Console.WriteLine("Entrez le nombre de valeurs :");
+        Nb = Convert.ToInt16(Console.ReadLine());
+        T = new int[Nb];
+        for (i = 0; i <= Nb - 1; i++)
+        {
+            Console.WriteLine("Entrez le nombre n° " + (i + 1));
+            T[i] = Convert.ToInt16(Console.ReadLine());
+        }
+        Flag = true;
+        for (i = 1; i <= Nb - 1; i++)
+        {
+            if (T[i] != T[i - 1] + 1)
+            {
+                Flag = false;
+            }
+        }
+        if (Flag)
+        {
+            Console.WriteLine("Les nombres sont consécutifs");
+        }
+        else
+        {
+            Console.WriteLine("Les nombres ne sont pas consécutifs");
+        }
+    }
+}

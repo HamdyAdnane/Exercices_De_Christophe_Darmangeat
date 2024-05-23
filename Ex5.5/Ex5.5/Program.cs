098668ccb17e1ee0c5a502039bd3c6b7257d476d
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:19:45 +0100

    Add files via upload

---
+++ Ex5.5/Ex5.5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+        Console.WriteLine("Entrez un nombre : ");
+        N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("La table de multiplication de ce nombre est : ");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(N+ " x "+ i+ " = "+ N * i);
+        }
+    }
+}

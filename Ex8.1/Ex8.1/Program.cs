e671cb2478b02add1e232a36585a1a34a1751cd8
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 13:23:11 +0100

    Add files via upload

---
+++ Ex8.1/Ex8.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] Truc = new int[5,12];
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; i < 12; j++)
+            {
+                Truc[i, j] = 0;
+            }
+        }
+    }
+}

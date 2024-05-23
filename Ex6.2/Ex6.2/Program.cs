18f9e40c5ec5374ba41e1aa50ccc9a7b0c8a6818
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:30:16 +0100

    Add files via upload

---
+++ Ex6.2/Ex6.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] Truc = new string[5];
+        Truc[0] = "a";
+        Truc[1] = "e";
+        Truc[2] = "i";
+        Truc[3] = "o";
+        Truc[4] = "u";
+        Truc[5] = "y";
+    }
+}

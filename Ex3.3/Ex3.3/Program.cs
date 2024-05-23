ac40f1e1aefffe60620e5d966c567d4a59e5ee42
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:01:17 +0100

    Add files via upload

---
+++ Ex3.3/Ex3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string a, b, c;
+        Console.WriteLine("Entrez successivement trois noms : ");
+        a = Console.ReadLine();
+        b = Console.ReadLine();
+        c = Console.ReadLine();
+        for (int i = 0; i < b.Length; i++)
+        {
+            if ((a[i] <= b[i]) && (b[i] <= c[i]))
+            {
+                Console.WriteLine("Ces noms sont classés alphabétiquement");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ces noms ne sont pas classés");
+                break;
+            }
+        }
+        
+
+    }
+}

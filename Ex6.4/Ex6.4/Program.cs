1998a964e6b37f3feed96a859ea45d7d9669b4a2
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:31:28 +0100

    Add files via upload

---
+++ Ex6.4/Ex6.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Nb = new int[5];
+        for (int i = 0; i < 5; i++)
+        {
+            Nb[i] = i * i;
+            Console.WriteLine(Nb[i]);
+        }
+    }
+}

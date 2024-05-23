a99e1fa745c9c0c011ff1bf138e7dda7535802ce
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:21:43 +0100

    Add files via upload

---
+++ Ex5.8/Ex5.8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N, PG,IPG;
+        PG = 0;
+        IPG = 0;
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine("Entrez un nombre : ");
+            N = Convert.ToInt32(Console.ReadLine());
+            if (i == 1 || N > PG)
+            {
+                PG = N;
+                IPG = i;
+            }
+        }
+        Console.WriteLine("Le nombre le plus grand était : "+ PG);
+        Console.WriteLine("Il a été saisi en position numéro "+ IPG);
+    }
+}

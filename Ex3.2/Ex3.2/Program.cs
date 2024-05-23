35fa3744b85d9b028f3237fb54f349af743db7e1
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:00:20 +0100

    Add files via upload

---
+++ Ex3.2/Ex3.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m, n;
+        Console.WriteLine("Entrez deux nombres : ");
+        n = Convert.ToInt16(Console.ReadLine());
+        m = Convert.ToInt16(Console.ReadLine());
+
+        if ((m > 0 && n > 0) || (m < 0 && n < 0))
+        {
+            Console.WriteLine("Leur produit est positif");
+        }
+        else
+        {
+            Console.WriteLine("Leur produit est négatif");
+        }
+    }
+}

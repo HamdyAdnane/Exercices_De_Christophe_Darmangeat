459bf8e3cf296e9fa76e1feda6ae75674c1966c9
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 16:42:45 +0100

    Add files via upload

---
+++ Ex2.2/Ex2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int Nombre, Carré;
+        Console.WriteLine("Entrez un nombre :");
+        Nombre = Convert.ToInt16(Console.ReadLine());
+        Carré = Nombre * Nombre;
+        Console.WriteLine("Son carré est : "+ Carré);
+    }
+}

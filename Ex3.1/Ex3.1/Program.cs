7af66a3e1ab62eb50c77a64207c3250c27a62cf9
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 11:59:09 +0100

    Add files via upload

---
+++ Ex3.1/Ex3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Entrer un nombre: ");
+        n = Convert.ToInt16(Console.ReadLine());
+        if (n > 0)
+        {
+            Console.WriteLine("Ce Nombre est positif");
+        }
+        else
+        {
+            Console.WriteLine("Ce Nombre est Negatif");
+        }
+    }
+}

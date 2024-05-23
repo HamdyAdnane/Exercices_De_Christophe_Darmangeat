7be7c02f0b91a55457081a2a96e8f3deb0887458
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 13:11:58 +0100

    Add files via upload

---
+++ Ex3.6/Ex3.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age;
+        Console.WriteLine("Entrez l’âge de l’enfant : ");
+        age = Convert.ToInt32(Console.ReadLine());
+        if (age >= 12)
+        {
+            Console.WriteLine("Catégorie Cadet");
+        }
+        else if (age >= 10)
+        {
+            Console.WriteLine("Catégorie Minime");
+        }
+        else if (age >= 8)
+        {
+            Console.WriteLine("Catégorie Pupille");
+        }
+        else if(age >= 6)
+        {
+            Console.WriteLine("Catégorie Poussin");
+        }
+    }
+}

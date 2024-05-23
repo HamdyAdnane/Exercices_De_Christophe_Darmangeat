8016f2dfc14c59c72a63db03514462fb87b94fe6
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:20:52 +0100

    Add files via upload

---
+++ Ex5.6/Ex5.6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N,Somme;
+        Console.WriteLine("Entrez un nombre : ");
+        N = Convert.ToInt32(Console.ReadLine());
+        Somme = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            Somme=Somme+i;
+        }
+        Console.WriteLine("La somme est : " + Somme);
+    }
+}
---
+++ Ex5.7/Ex5.7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N, F;
+        Console.WriteLine("Entrez un nombre : ");
+        N = Convert.ToInt32(Console.ReadLine());
+        F = 1;
+        for (int i = 2; i <= N; i++)
+        {
+            F = F* i;
+        }
+        Console.WriteLine("La factorielle est : "+ F);
+    }
+}

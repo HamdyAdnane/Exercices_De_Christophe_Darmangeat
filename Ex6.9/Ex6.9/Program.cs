29b40bfc3b3f855b2937944897d8def00b6e3047
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:34:33 +0100

    Add files via upload

---
+++ Ex6.9/Ex6.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Som, Nb;
+        int[] T;
+        Console.WriteLine("Entrez le nombre de valeurs :");
+        Nb = Convert.ToInt16(Console.ReadLine());
+        T = new int[Nb];
+        Som = 0;
+        for (int i = 0; i <= Nb - 1; i++)
+        {
+            Console.WriteLine("Entrez le nombre n° " + (i + 1));
+            T[i] = Convert.ToInt16(Console.ReadLine());
+            Som = Som + T[i];
+        }
+        Console.WriteLine("Somme des éléments du tableau : "+ Som);
+    }
+}

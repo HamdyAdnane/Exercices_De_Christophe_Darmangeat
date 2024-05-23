55e85045b621899d9f626f91fec21e552c101342
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:33:24 +0100

    Add files via upload

---
+++ Ex6.7/Ex6.7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Notes = new int[9];
+        int S = 0;
+
+        for (int i = 0; i <= 8; i++)
+        {
+            Console.WriteLine("Entrez la note n° "+ (i + 1));
+            Notes[i] = Convert.ToInt16(Console.ReadLine());
+            S = S + Notes[i];
+        }
+        Console.WriteLine("Moyenne :"+ (S / 9));
+    }
+}

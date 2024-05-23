49482d9e3b9020c5aba5ebd3aed264a935c3b0c3
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:25:25 +0100

    Add files via upload

---
+++ Ex6.1/Ex6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Truc = new int[6];
+        for (int i = 0; i < 6; i++) 
+        {
+            Truc[i] = 0;
+        }
+    }
+}

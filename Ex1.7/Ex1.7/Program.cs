4a8b0095b6054c7941747e66fe2b3a3cb2acc7dc
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 16:37:30 +0100

    Add files via upload

---
+++ Ex1.7/Ex1.7/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B, C, Changer;
+        Console.WriteLine("Entrez le 1er valeur :");
+        A = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Entrez la 2eme valeur :");
+        B = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Entrez la 2eme valeur :");
+        C = Convert.ToInt16(Console.ReadLine());
+        Changer = C;
+        C = B;
+        B = A;
+        A = Changer;
+    }
+}

36541a0e4d9c50a49e138ea389269d7f699cf099
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:02:12 +0100

    Add files via upload

---
+++ Ex3.4/Ex3.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int n;
+        Console.WriteLine("Entrez un nombre : ");
+        n = Convert.ToInt32(Console.ReadLine());
+        if (n < 0)
+        {
+            Console.WriteLine("Ce nombre est négatif");
+        }else if (n == 0)
+        {
+            Console.WriteLine("Ce nombre est nul");
+        }
+        else
+        {
+            Console.WriteLine("Ce nombre est positif");
+        }
+    }
+    }
+
---
+++ Ex3.5/Ex3.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m, n;
+        Console.WriteLine("Entrez deux nombres : ");
+        n = Convert.ToInt16(Console.ReadLine());
+        m = Convert.ToInt16(Console.ReadLine());
+
+        if ((n == 0) || (m == 0))
+        {
+            Console.WriteLine("Le produit est nul");
+        }
+        else if ((m > 0 && n > 0) || (m < 0 && n < 0))
+        {
+            Console.WriteLine("Leur produit est positif");
+        }
+        else
+        {
+            Console.WriteLine("Leur produit est négatif");
+        }
+    }
+}

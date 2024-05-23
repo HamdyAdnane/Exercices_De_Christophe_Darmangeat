7c12adc789883b03768f0ff634a32411f9761810
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:50:20 +0100

    Add files via upload

---
+++ Ex7.3/Ex7.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Nb, i, temp;
+        int[] t;
+        Console.WriteLine("Entrez le nombre de valeurs :");
+        Nb = Convert.ToInt16(Console.ReadLine());
+        t = new int[Nb];
+        for (i = 0; i <= Nb - 1; i++)
+        {
+            Console.WriteLine("Entrez le nombre n° " + (i + 1));
+            t[i] = Convert.ToInt16(Console.ReadLine());
+        }
+        for (i = 0; i <= (Nb - 1) / 2; i++)
+        {
+            temp = t[i];
+            t[i] = t[Nb - 1 - i];
+            t[Nb - 1 - i] = temp;
+        }
+    }
+}

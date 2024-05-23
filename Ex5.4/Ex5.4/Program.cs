64a34ab0a62b61133288c77c49c24ad2df2ece7f
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:18:49 +0100

    Add files via upload

---
+++ Ex5.4/Ex5.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+        Console.WriteLine("Entrez un nombre : ");
+        N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Les 10 nombres suivants sont : ");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(N + i);
+        }
+    }
+}

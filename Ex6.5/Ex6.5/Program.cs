560d197685a9b1f7de959aa338077f8df6a2b83d
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:32:10 +0100

    Add files via upload

---
+++ Ex6.5/Ex6.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] N = new int[7];
+        N[0] = 1;
+        Console.WriteLine(N[0]);
+        for (int k = 1; k <= 7; k++)
+        {
+            N[k] = N[k - 1] + 2;
+            Console.WriteLine(N[k]);
+        }
+    }
+}

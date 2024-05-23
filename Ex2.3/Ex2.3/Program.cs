8ef85ff1e1b081826a821c9f66f6d2b20a8f9da1
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 16:46:41 +0100

    Add files via upload

---
+++ Ex2.3/Ex2.3/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string Prenom;
+        Console.WriteLine("Quel est votre prenom ?");
+        Prenom = Console.ReadLine();
+        Console.WriteLine("Bonjour "+ Prenom+ " !");
+    }
+}

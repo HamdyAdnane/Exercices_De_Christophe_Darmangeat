dc161e738f761100bd5d14e6593aacf09772fcdf
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:23:02 +0100

    Add files via upload

---
+++ Ex5.10/Ex5.10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int E, somdue, M, Reste, Nb10E, Nb5E;
+        E = 1;
+        somdue = 0;
+        while (E != 0)
+        {
+            Console.WriteLine("Entrez le montant : ");
+            E = Convert.ToInt32(Console.ReadLine());
+            somdue = somdue + E;
+        }
+        Console.Write("Vous devez :"+ somdue+ " euros ,");
+        Console.WriteLine("Montant versé :");
+        M = Convert.ToInt32(Console.ReadLine());
+        Reste = M - somdue;
+        Nb10E = 0;
+        while (Reste >= 10)
+        {
+            Nb10E = Nb10E + 1;
+            Reste = Reste - 10;
+        }
+        Nb5E = 0;
+        if (Reste >= 5)
+        {
+            Nb5E = 1;
+            Reste = Reste - 5;
+        }
+        Console.WriteLine("Rendu de la monnaie :");
+        Console.WriteLine("Billets de 10 E : " + Nb10E);
+        Console.WriteLine("Billets de  5 E : " + Nb5E);
+        Console.WriteLine("Pièces de 1 E : " + Reste);
+    }
+}
---
+++ Ex5.11/Ex5.11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N, P, i, A, B;
+        Console.WriteLine("Entrez le nombre de chevaux partants : ");
+        N =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Entrez le nombre de chevaux joués : ");
+        P = Convert.ToInt32(Console.ReadLine());
+        A = 1;
+        B = 1;
+        for (i = 1; i <= P; i++)
+        {
+            A = A * (i + N - P);
+            B = B * i;
+        }
+        Console.WriteLine("Dans l’ordre, une chance sur " + A);
+        Console.WriteLine("Dans le désordre, une chance sur "+ A / B);
+    }
+}

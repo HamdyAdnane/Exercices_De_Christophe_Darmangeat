cba9e051fb8fd72c159768128d2bb58d7a60d57a
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-23 12:30:56 +0100

    Add files via upload

---
+++ Ex6.3/Ex6.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Notes = new int[8];
+        for (int i = 0; i < 8; i++)
+        {
+            Console.WriteLine("Entrez la note numéro "+ (i + 1));
+            Notes[i]=Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}   

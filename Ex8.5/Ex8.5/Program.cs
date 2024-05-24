93d5b2910438bed75f51e874e8c1a6331e03cbcb
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-24 10:57:30 +0100

    Add files via upload

---
+++ Ex8.5/Ex8.5/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] T = new int[4, 2];
+        int k, m;
+        // Partie de T[k, m] ← 2 * k + [m + 1]
+        for (k = 0; k < 4; k++)
+        {
+            for (m = 0; m < 2; m++)
+            {
+                T[k, m] = (2 * k) + (m + 1);
+            }
+        }
+        for (k = 0; k < 4; k++)
+        {
+            for (m = 0; m < 2; m++)
+            {
+                Console.WriteLine(T[k, m]);
+            }
+        }
+        // Partie de T[k, m] ← [k + 1] + 4 * m
+        for (k = 0; k < 4; k++)
+        {
+            for (m = 0; m < 2; m++)
+            {
+                T[k, m] = (k + 1) + (4 * m);
+            }
+        }
+        for (k = 0; k < 4; k++)
+        {
+            for (m = 0; m < 2; m++)
+            {
+                Console.WriteLine(T[k, m]);
+            }
+        }
+    }
+}

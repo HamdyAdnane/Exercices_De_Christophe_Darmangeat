9bdb58e2ed32eea044d5a2fe17b663ead9e658d3
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-24 09:51:56 +0100

    Add files via upload

---
+++ Ex8.3/Ex8.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] X = new int[1, 2];
+        int i, j, Val;
+        Val = 1;
+        for (i = 0; i <= 1; i++)
+        {
+            for (j = 0; i <= 2; j++)
+            {
+                X[i, j] = Val;
+                Val++;
+            }
+        }
+        for (j = 0; i <= 2; j++)
+        {
+            for (i = 0; i <= 1; i++)
+            {
+                Console.WriteLine(X[i, j]);
+            }
+        }
+
+    }
+}

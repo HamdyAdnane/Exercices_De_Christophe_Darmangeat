c08d2bdee82432e396382e875cdbb8835ab693f1
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-24 10:28:36 +0100

    Add files via upload

---
+++ Ex8.4/Ex8.4/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] T = new int[4, 2];
+        int k ,m ;
+        for (k = 0; k < 4; k++)
+        {
+            for (m = 0; m < 2; m++)
+            {
+                T[k, m] = k+m;
+            }
+        }
+        for (k = 0; k < 4; k++)
+        {
+            for (m = 0; m < 2; m++)
+            {
+                Console.WriteLine(T[k, m]);
+            }
+        }
+
+    }
+}

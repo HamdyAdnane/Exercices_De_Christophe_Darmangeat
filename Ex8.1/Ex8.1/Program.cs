6c4e4a909cc9393c7aabe3cc9d2c92d5ccf043c3
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-24 09:35:00 +0100

    Update Program.cs

--- Ex8.1/Ex8.1/Program.cs
+++ Ex8.1/Ex8.1/Program.cs
@@ -4,9 +4,9 @@ class Program
     static void Main(string[] args)
     {
         int[,] Truc = new int[5,12];
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i <= 5; i++)
         {
-            for (int j = 0; i < 12; j++)
+            for (int j = 0; i <= 12; j++)
             {
                 Truc[i, j] = 0;
             }

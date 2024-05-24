bd52cfc169c87a10a74de30d44522686f9fbca2e
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-24 10:50:50 +0100

    Update Program.cs

--- Ex8.1/Ex8.1/Program.cs
+++ Ex8.1/Ex8.1/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
 class Program
 {
     static void Main(string[] args)
     {
-        int[,] Truc = new int[5,12];
-        for (int i = 0; i <= 5; i++)
+        int[,] T = new int[6, 13];
+        int i, j, Val;
+        Val = 1;
+        for (i = 0; i <= 5; i++)
         {
-            for (int j = 0; i <= 12; j++)
+            for (j = 0; j <= 12; j++)
             {
-                Truc[i, j] = 0;
+                T[i, j] = Val;
+                Val++;
+                Console.WriteLine(T[i, j]);
             }
         }
     }

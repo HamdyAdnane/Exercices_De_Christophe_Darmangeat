b05ecae8126cb2fe5325de68cece56d193576dd9
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-28 13:59:15 +0100

    Add files via upload

---
+++ Ex11.1/Ex11.1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int Nombre1, Nombre2, Nombre3, Nombre4, Nombre5;
+        Console.WriteLine("Entrer Cinq Nombres");
+        Nombre1 = Convert.ToInt32(Console.ReadLine());
+        Nombre2 = Convert.ToInt32(Console.ReadLine());
+        Nombre3 = Convert.ToInt32(Console.ReadLine());
+        Nombre4 = Convert.ToInt32(Console.ReadLine());
+        Nombre5 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("La Somme de C'est Nombre est :" +Somme(Nombre1,Nombre2,Nombre3,Nombre4,Nombre5));
+    }
+    private static int Somme(int a, int b, int c, int d, int e)
+    {
+        return a+b+c+d+e;
+    }
+}

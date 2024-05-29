c11d4a8f0ce4900213606fad336a368fdbca80df
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-29 11:29:50 +0100

    Add files via upload

---
+++ Ex11.7/Ex11.7/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] Tableau;
+        int Nombre_elements;
+        Console.WriteLine("Entrer Le Nombre Des élements Que Tu as Entrée :");
+        Nombre_elements = Convert.ToInt32(Console.ReadLine());
+        Tableau = new int[Nombre_elements];
+        for (int i = 0; i <= Nombre_elements - 1; i++)
+        {
+            Console.WriteLine("Entrez l'élement n° " + (i + 1));
+            Tableau[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        bool situation = TableauCroissant(Nombre_elements, Tableau);
+        Console.WriteLine("La Croissant De Ces élemnts est "+situation);
+    }
+    public static bool TableauCroissant(int Taille_Tableau, int[] Tableau)
+    {
+        int i = 0;
+        bool Flag = true;
+        while (Flag && i< Taille_Tableau-1)
+        {
+            Flag = Tableau[i] < Tableau[i+1];
+            i++;
+        }
+        return Flag;
+    }
+}

6bc6e68823c962cd3f67fdc116699a385d347d94
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-29 10:33:13 +0100

    Add files via upload

---
+++ Ex9.2/Ex9.2/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string Mot;
+        Console.WriteLine("Entrer un mot , Stp!");
+        Mot = Console.ReadLine();
+        Nombre_Caractere(Mot);
+    }
+    private static void Nombre_Caractere(string Mot)
+    {
+        int ic = 0;
+        while (ic < Mot.Length)
+        {
+            if (Mot != null)
+            {
+                ic++;
+            }
+        }
+        Console.WriteLine("Ce mot compte "+ ic + " lettres");
+    }
+}
+

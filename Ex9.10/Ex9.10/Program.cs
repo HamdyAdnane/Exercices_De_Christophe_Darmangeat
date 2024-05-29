a01deb7893af95700b54eb124b1012abb777797a
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-29 10:56:04 +0100

    Add files via upload

---
+++ Ex9.10/Ex9.10/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int nombre;
+        Console.WriteLine("Entrez votre nombre : ");
+        nombre = Convert.ToInt32(Console.ReadLine());
+        Pair(nombre);
+    }
+    private static void Pair(int Nb)
+    {
+        if (Nb % 2 == 0)
+        {
+            Console.WriteLine("Le Nombre est pair");
+        }
+        else
+        {
+            Console.WriteLine("Le Nombre est impair");
+        }
+    }
+}

edcc2b5818a9a23b19838b71f875f0d14967e9ad
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-05-30 11:22:15 +0100

    Add files via upload

---
+++ Ex11.8/Ex11.8/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int Nombre1, Nombre2;
+        Console.WriteLine("Entrer Deux Nombres Stp!");
+        Nombre1 = Convert.ToInt32(Console.ReadLine());
+        Nombre2 = Convert.ToInt32(Console.ReadLine());
+        Inversion(Nombre1, Nombre2);
+    }
+    public static void Inversion(int Valeur1, int Valeur2)
+    {
+        int Changer;
+        Changer = Valeur1;
+        Valeur1 = Valeur2;
+        Valeur2 = Changer;
+    }
+}

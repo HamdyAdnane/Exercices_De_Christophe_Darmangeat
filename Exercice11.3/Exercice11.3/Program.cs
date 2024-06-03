bb6e301f2766eef00f29936ccb7f455ff7eb0015
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-06-03 15:32:05 +0200

    Add files via upload

---
+++ Exercice11.3/Exercice11.3/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Trouve("je m'appel adnane hamdy", "y"));
+    }
+    public static int Trouve(string Phrase, string Caractere2)
+    {
+        int i = 1;
+        while (i < Phrase.Length - Caractere2.Length && Caractere2 != Phrase.Substring(i, Caractere2.Length) )
+        {
+            i++;
+        }
+        if (Caractere2 != Phrase.Substring(i, Caractere2.Length))
+        {
+            return 0;
+        }
+        return i;
+    }
+}

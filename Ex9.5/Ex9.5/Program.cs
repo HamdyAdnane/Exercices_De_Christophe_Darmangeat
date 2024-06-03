4a6eb0c819f5baebe8f013b992270ab3415fff08
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-06-03 17:40:22 +0200

    Add files via upload

---
+++ Ex9.5/Ex9.5/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string Phrase, Nouvelle_Phrase;
+        int Indice_Caractere_Supprimer;
+        Console.WriteLine("Entrez une phrase : ");
+        Phrase = Console.ReadLine();
+        Console.WriteLine("Entrez le rang du caractère à supprimer : ");
+        Indice_Caractere_Supprimer = Convert.ToInt32(Console.ReadLine());
+        Nouvelle_Phrase = Phrase.Remove((Indice_Caractere_Supprimer-1), 1);
+        Console.WriteLine("La nouvelle phrase est :"+ Nouvelle_Phrase);
+    }
+}

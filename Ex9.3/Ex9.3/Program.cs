79d33b6fbb3c6b380799e821374c4307c21a4b81
Hamdy Adnane <149180855+HamdyAdnane@users.noreply.github.com>
2024-06-03 12:44:18 +0200

    Add files via upload

---
+++ Ex9.3/Ex9.3/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main(string [] args)
+    {
+        string Phrase;
+        int Nombres_Mots;
+        Console.WriteLine("Entrez une phrase : ");
+        Phrase = Console.ReadLine();
+        Nombres_Mots = 0;
+        for(int i = 0; i< Phrase.Length; i++)
+        {
+            if(Phrase.Substring(i,1) == " ")
+            {
+                Nombres_Mots++;
+            }
+        }
+        Console.Write("Cette phrase compte " +(Nombres_Mots + 1) + " mots");
+    }
+}
